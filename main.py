5233ecaf35a9af6aa505c6557963d7d26595f3b6
Marc-Antoine Nadeau <nadeau.marc-antoine@hotmail.com>
2025-04-03 19:38:49 -0400

    dev

--- main.py
+++ main.py
@@ -23,23 +23,23 @@ from tradingSimulator import TradingSimulator
 if(__name__ == '__main__'):
 
     # Retrieve the paramaters sent by the user
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument("-strategy", default='TDQN', type=str, help="Name of the trading strategy")
-    parser.add_argument("-stock", default='Apple', type=str, help="Name of the stock (market)")
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description='')
+    # parser.add_argument("-strategy", default='TDQN', type=str, help="Name of the trading strategy")
+    # parser.add_argument("-stock", default='Apple', type=str, help="Name of the stock (market)")
+    # args = parser.parse_args()
     
-    # Initialization of the required variables
+    # # Initialization of the required variables
     simulator = TradingSimulator()
-    strategy = args.strategy
-    stock = args.stock
+    # strategy = args.strategy
+    # stock = args.stock
 
-    # Training and testing of the trading strategy specified for the stock (market) specified
-    simulator.simulateNewStrategy(strategy, stock, saveStrategy=False)
-    """
+    # # Training and testing of the trading strategy specified for the stock (market) specified
+    # simulator.simulateNewStrategy(strategy, stock, saveStrategy=False)
+    
     simulator.displayTestbench()
-    simulator.analyseTimeSeries(stock)
-    simulator.simulateNewStrategy(strategy, stock, saveStrategy=False)
-    simulator.simulateExistingStrategy(strategy, stock)
-    simulator.evaluateStrategy(strategy, saveStrategy=False)
-    simulator.evaluateStock(stock)
-    """
+    # simulator.analyseTimeSeries(stock)
+    # simulator.simulateNewStrategy(strategy, stock, saveStrategy=False)
+    # simulator.simulateExistingStrategy(strategy, stock)
+    # simulator.evaluateStrategy(strategy, saveStrategy=False)
+    # simulator.evaluateStock(stock)
+    
